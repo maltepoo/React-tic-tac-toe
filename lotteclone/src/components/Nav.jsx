a6edc5569eed312df87e46f47b7bf14959374575
jeongpin <xooxop@gmail.com>
2022-02-01 16:03:43 +0900

    ✒ Add: scroll nav

---
+++ lotteclone/src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Nav() {
+    const displayNav = 420;
+    return (
+        <div className='depth2_wrap'>
+            <div className='depth2_wrap_in'>
+                <div className='link_depth2_logo'>
+                    <a></a>
+                </div>
+                <ul className='depth2'>
+                    <li><a className='link02'>혜택&이벤트</a></li>
+                    <li><a className='link02'>매거진</a></li>
+                    <li><a className='link02'>EAT 플레이스</a></li>
+                    <li><a className='link02'>지점/매장안내</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
